421d02e91799cd12952f357f5a5544d5815d7be8
Farhan <patwary.farhan87@gmail.com>
2019-03-02 15:34:19 +0000

    added more stuff

--- src/lib.rs
+++ src/lib.rs
@@ -18,20 +18,47 @@ impl Sob<usize> {
     pub fn new() -> Sob {
         Sob { vob: Vob::new() }
     }
-
+    /// ```
+    /// use sob::Sob;
+    ///
+    /// let s = Sob::with_capacity(100);
+    /// assert!(s.capacity() >= 100);
+    /// ```
     pub fn with_capacity(len: usize) -> Sob {
         let vob = Vob::from_elem(len, false);
         Sob::from_vob(vob)
     }
 
+    /// Creates a sob with capacity from an existing vob
+    /// # Examples
+    ///
+    /// ```
+    /// use sob::Sob;
+    /// use vob::Vob;
+    /// let v = Vob::new();
+    /// let mut x = Sob::from_vob(v);
+    /// ```
     pub fn from_vob(x: Vob) -> Sob {
         Sob { vob: x }
     }
 
+    /// Returns the capacity in bits for the sob
+    /// # Examples
+    /// ```
+    /// use sob::Sob;
+    /// use vob::Vob;
+    /// let v = Vob::new();
+    /// let mut x = Sob::from_vob(v);
+    /// assert_eq!(x.capacity(),0);
+    /// ```
     pub fn capacity(&self) -> usize {
         self.vob.len()
     }
 
+    /// Reserves the minimum capacity
+    /// for the given BitSet to contain len distinct elements.
+    /// In the case of BitSet this means reallocations will not
+    /// occur as long as all inserted elements are less than len.
     pub fn reserve_len(&mut self, len: usize) {
         let cur_len = self.vob.len();
         if len >= cur_len {
@@ -50,12 +77,43 @@ impl Sob<usize> {
     pub fn shrink_to_fit(&mut self) {
         self.vob.shrink_to_fit()
     }
-}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    pub fn len(&self) -> usize {
+        self.vob.iter_set_bits(..).count()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.vob.is_empty()
+    }
+
+    pub fn clear(&mut self) {
+        self.vob.clear();
+    }
+
+    pub fn contains(&self, value: &usize) -> bool {
+        self.vob.get(*value).unwrap()
+    }
+
+    pub fn insert(&mut self, value: usize) -> bool {
+        if self.contains(&value) {
+            return false;
+        }
+
+        let len = self.vob.len();
+        if value >= len {
+            self.vob.resize(value - len + 1, false)
+        }
+
+        self.vob.set(value, true);
+        return true;
+    }
+
+    pub fn remove(&mut self, value: &usize) -> bool {
+        if !self.contains(value) {
+            return false;
+        }
+
+        self.vob.set(*value, false);
+
+        return true;
     }
 }
