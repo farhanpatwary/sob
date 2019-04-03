ad28488fb788a446eb2a19192aec701e4e2482e0
Farhan Patwary <patwary.farhan87@gmail.com>
2019-04-03 17:52:12 +0100

    added more tests and improved comments

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub struct Sob {
 }
 
 impl Sob {
-    /// Creates an empty Sob
+    /// Creates an empty Sob.
     /// # Examples
     ///
     /// ```
@@ -18,7 +18,7 @@ impl Sob {
         Sob { vob: Vob::new() }
     }
 
-    /// Creates a sob with capacity len
+    /// Creates a sob with capacity len.
     /// ```
     /// use sob::Sob;
     ///
@@ -30,7 +30,7 @@ impl Sob {
         Sob::from_vob(vob)
     }
 
-    /// Creates a sob from an existing vob
+    /// Creates a sob from an existing vob.
     /// # Examples
     ///
     /// ```
@@ -43,7 +43,7 @@ impl Sob {
         Sob { vob: x }
     }
 
-    /// Returns the capacity in bits for the sob
+    /// Returns the capacity in bits for the sob.
     /// # Examples
     /// ```
     /// use sob::Sob;
@@ -59,9 +59,7 @@ impl Sob {
     }
 
     /// Reserves the minimum capacity
-    /// for the given BitSet to contain len distinct elements.
-    /// In the case of BitSet this means reallocations will not
-    /// occur as long as all inserted elements are less than len.
+    /// for the given sob to contain len distinct elements.
     /// # Examples
     ///
     /// ```
@@ -74,13 +72,12 @@ impl Sob {
     /// ```
     pub fn reserve_len(&mut self, len: usize) {
         let cur_len = self.vob.len();
-        println!("cur {:?}", cur_len);
         if len >= cur_len {
             self.vob.resize(len - cur_len, false);
         }
     }
 
-    /// Consumes this set to return the underlying bit vector.
+    /// Returns the underlying vob.
     ///
     /// # Examples
     ///
@@ -102,7 +99,7 @@ impl Sob {
     pub fn get(self, index: usize) -> Option<bool> {
         self.vob.get(index)
     }
-    /// Returns a reference to the underlying bit vector.
+    /// Returns a reference to the underlying vob.
     ///
     /// # Examples
     ///
@@ -110,15 +107,15 @@ impl Sob {
     /// use sob::Sob;
     ///
     /// let mut s = Sob::new();
-    /// s.insert(0);
+    /// s.insert(10);
     ///
     /// let bv = s.get_ref();
-    /// assert_eq!(bv[0], true);
+    /// assert_eq!(bv[10], true);
     /// ```
     pub fn get_ref(&self) -> &Vob {
         &self.vob
     }
-    /// Truncates the underlying vector to the least length required.
+    /// Truncates the vob so that it has the least length required.
     ///
     /// # Examples
     ///
@@ -126,31 +123,62 @@ impl Sob {
     /// use sob::Sob;
     ///
     /// let mut s = Sob::new();
-    /// s.insert(32183231);
-    /// s.remove(&32183231);
-    ///
-    /// // Internal storage will probably be bigger than necessary
-    /// println!("old capacity: {}", s.capacity());
-    ///
-    /// // Now should be smaller
+    /// s.insert(10);
+    /// // The underlying vob should now have a length of at least 10.
+    /// s.remove(&10);
+    /// // The vob no longer needs a length of 10 so it can be shrunk.
     /// s.shrink_to_fit();
-    /// println!("new capacity: {}", s.capacity());
+    /// // The vob now has a smaller length.
     /// ```
     pub fn shrink_to_fit(&mut self) {
         self.vob.shrink_to_fit()
     }
-
-    pub fn len(&self) -> usize {
-        self.vob.len()
-    }
+    /// Returns whether the Sob is empty or not.
+    /// True if the Sob is empty, false if it isn't empty.
+    /// # Examples
+    ///
+    /// ```
+    /// use sob::Sob;
+    ///
+    /// let mut s = Sob::new();
+    /// assert_eq!(s.is_empty(),true);
+    /// ```
     pub fn is_empty(&self) -> bool {
-        self.vob.is_empty()
+        if self.vob.iter_set_bits(..).count() == 0 {
+            true
+        } else {
+            false
+        }
     }
-
+    /// Empties the Sob.
+    /// Returns whether the Sob is empty or not.
+    /// True if the Sob is empty, false if it isn't empty.
+    /// # Examples
+    ///
+    /// ```
+    /// use sob::Sob;
+    ///
+    /// let mut s = Sob::new();
+    /// s.insert(10);
+    /// assert_eq!(s.is_empty(),false);
+    /// s.clear();
+    /// assert_eq!(s.is_empty(),true);
+    /// ```
     pub fn clear(&mut self) {
         self.vob.clear();
     }
-
+    /// Checks the Sob for value.
+    /// Returns false if the value doesn't exist.
+    /// Otherwise, it returns the value stored.
+    /// # Examples
+    ///
+    /// ```
+    /// use sob::Sob;
+    ///
+    /// let mut s = Sob::new();
+    /// s.insert(10);
+    /// assert_eq!(s.contains(&10),true);
+    /// ```
     pub fn contains(&self, value: &usize) -> bool {
         if (self.vob.get(*value)).is_none() == true {
             false
@@ -158,7 +186,19 @@ impl Sob {
             self.vob.get(*value).unwrap()
         }
     }
-
+    /// Inserts value into the Sob.
+    /// Returns false if the Sob already contains the value.
+    /// Otherwise, it inserts the value and returns true.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use sob::Sob;
+    ///
+    /// let mut s = Sob::new();
+    /// assert_eq!(s.insert(10),true);
+    /// assert_eq!(s.insert(10),false);
+    /// ```
     pub fn insert(&mut self, value: usize) -> bool {
         if self.contains(&value) {
             return false;
@@ -172,7 +212,19 @@ impl Sob {
         self.vob.set(value, true);
         return true;
     }
-
+    /// Removes value from Sob.
+    /// Returns false if the Sob doesn't contain the value
+    /// Otherwise, it removes value from sob and returns true.
+    /// # Examples
+    ///
+    /// ```
+    /// use sob::Sob;
+    ///
+    /// let mut s = Sob::new();
+    /// s.insert(10);
+    /// assert_eq!(s.remove(&10),true);
+    /// assert_eq!(s.remove(&10),false);
+    /// ```
     pub fn remove(&mut self, value: &usize) -> bool {
         if !self.contains(value) {
             return false;
